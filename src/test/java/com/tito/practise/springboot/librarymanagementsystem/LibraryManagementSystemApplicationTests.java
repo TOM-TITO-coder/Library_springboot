1f4a8fd1bb4ae5eae48af4df29d402a153e67613
TOM-TITO <126035168+Tito-com@users.noreply.github.com>
2023-08-29 08:35:53 +0700

    initial commit

---
+++ src/test/java/com/tito/practise/springboot/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tito.practise.springboot.librarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
